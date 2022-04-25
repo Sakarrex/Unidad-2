1dfc1ddca905d29351ae8c2c5ca87d59b6fc8ccb
Sakarrex <sebasgarciafontana@gmail.com>
2022-04-25 11:57:18 -0300

    Ejercicio 3

--- Ejercicio3/Main.py
+++ Ejercicio3/Main.py
@@ -4,12 +4,19 @@ import csv
 from ModuloRegistro import Registro
 
 if __name__ == "__main__":
-    RegistroDia = []
+    
+    __RegistroDia = []
     archivo = open("c:\\Users\\Usuario\\Desktop\\POO\\Unidad-2\\Ejercicio3\\Registro.csv")
     reader = csv.reader(archivo, delimiter = ",")
     for files in reader:
-        RegistroDia.append( int(files[0]) , RegistroHora = [int(files[1]) , Registro(int(files[2]) , int(files[3]) , int(files[4]) )])
+        __RegistroHora = [int(files[1]),Registro(int(files[2]) , int(files[3]) , int(files[4]) )]
+        __RegistroDia.append(int(files[0]))
+        __RegistroDia.append(__RegistroHora)
     archivo.close
 
-    for i in RegistroDia:
-        print("Dia: {} Hora: {} Temperatura: {} Humedad: {} Presion: {}".format(RegistroDia[0],RegistroDia[1][0],RegistroDia[1][1].getTemperatura,RegistroDia[1][1].getHumedad,RegistroDia[1][1].getPresion))
+    i= 0
+
+    for i in range(len(__RegistroDia)):
+        print("Dia{}".format(__RegistroDia[i]))
+        ##print("Dia: {} Hora: {} Temperatura: {} Humedad: {} Presion: {}".format(__RegistroDia[0],__RegistroDia[1][0],__RegistroDia[1][1].getTemperatura,__RegistroDia[1][1].getHumedad,__RegistroDia[1][1].getPresion))
+        ##
---
+++ PruebaLista.py
@@ -0,0 +1,5 @@
+Lista = [[1,2],[2,3],[3,4]]
+i = 0
+for i in range(len(Lista)):
+    for j in range(len(Lista[i])):
+        print(Lista[i][j])
